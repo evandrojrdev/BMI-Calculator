144e95cdcb3ce80f4d229989b94ef2e1fbbb20fe
evandrojrdev <evandrojunior1517@gmail.com>
2021-06-01 22:14:34 +0100

    New update

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Calculo_Imc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txt_imc = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(80, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(374, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCULATE YOUR BMI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(80, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "HEIGHT:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(80, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "WEIGHT:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(80, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "YOUR BMI:";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(236, 75);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(205, 27);
+            this.txtPeso.TabIndex = 4;
+          
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(236, 122);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(205, 27);
+            this.txtAltura.TabIndex = 5;
+            // 
+            // txt_imc
+            // 
+            this.txt_imc.Location = new System.Drawing.Point(236, 169);
+            this.txt_imc.Name = "txt_imc";
+            this.txt_imc.Size = new System.Drawing.Size(205, 27);
+            this.txt_imc.TabIndex = 6;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.BackColor = System.Drawing.Color.Yellow;
+            this.btnVerificar.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnVerificar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnVerificar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnVerificar.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnVerificar.Location = new System.Drawing.Point(69, 427);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(135, 56);
+            this.btnVerificar.TabIndex = 7;
+            this.btnVerificar.Text = "CHECK THE BMI";
+            this.btnVerificar.UseVisualStyleBackColor = false;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Yellow;
+            this.btnLimpar.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLimpar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnLimpar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnLimpar.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLimpar.Location = new System.Drawing.Point(221, 427);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(131, 56);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "CLEAN";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Calculo_Imc.Properties.Resources._124_1248492_pratos_de_comida_png_food;
+            this.pictureBox1.Location = new System.Drawing.Point(69, 207);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(403, 205);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Yellow;
+            this.btnSair.FlatAppearance.CheckedBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSair.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnSair.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnSair.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSair.Location = new System.Drawing.Point(367, 427);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(116, 56);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "EXIT";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 505);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txt_imc);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculadora - IMC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txt_imc;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_Imc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double peso, altura, verif_imc;
+            peso = Convert.ToDouble(txtPeso.Text);
+            altura = Convert.ToDouble(txtAltura.Text);
+            verif_imc = peso/(altura * altura);
+
+            //MessageBox.Show("O seu IMC é:", Convert.ToString(txt_imc.Text));
+
+            //txt_imc.Text = verif_imc.ToString("0.00");
+            txt_imc.Text = Convert.ToString(Math.Round (verif_imc, 2));
+
+            if (verif_imc < 18.49)
+            {
+                //MessageBox.Show($"Seu IMC é {Math.Round(verif_imc, 2)}. Você está abaixo do peso.");
+                MessageBox.Show("SITUATION: You are underweight", "BMI calculation", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (verif_imc < 24.99)
+            {
+                //MessageBox.Show($"Parabéns! Seu IMC é {Math.Round(verif_imc, 2)}, e é o ideal.");
+                MessageBox.Show("You are with normal weight", "BMI calculation", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            else if (verif_imc < 29.99) 
+            {
+                MessageBox.Show("SITUATION: You are overweight", "BMI calculation", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (verif_imc < 34.99)
+            {
+                MessageBox.Show("SITUATION: You are overweight", "BMI calculation", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (verif_imc < 39.99)
+            {
+                MessageBox.Show("Attention! You have class II Obesity (severe).", "BMI calculation", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+                MessageBox.Show("Attention! You have class III obesity (morbid)", "BMI calculation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtAltura.Clear();
+            txtPeso.Clear();
+            txt_imc.Clear();
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        
+        
+
+        
+    }
+}
